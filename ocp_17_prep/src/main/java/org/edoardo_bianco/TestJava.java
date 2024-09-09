ecb4fff2f69b851029faf793c3b65e3b2f317f56
edoardo-bianco <edoardo.bianco@gmail.com>
2024-09-09 09:29:17 -0300

    alinhamento do codigo

---
+++ ocp_17_prep/src/main/java/org/edoardo_bianco/TestJava.java
@@ -0,0 +1,23 @@
+package org.edoardo_bianco;
+
+import java.util.Random;
+
+//Add any necessary import statement here
+public class TestJava {
+    public static void main(String [] args){
+        //add teste code here
+        float value = 102.0f;
+        //add any print statement here
+        System.out.println("Hello world: OCP 17");
+        Random r = new Random();
+        System.out.println("Random numeber: "+r.nextInt(9, 20));
+
+        String textBox = """
+                "Java study Guide "
+                  by SCott & Jeanne
+                """;
+        System.out.println("text Box : "+ textBox); 
+           
+
+    }
+}
